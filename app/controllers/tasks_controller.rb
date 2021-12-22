cfb82f9b4387133e1cb74175c808a61fcbf6ec9a
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-22 23:13:45 +0900

    タスクの新規作成画面、編集画面を作成する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,35 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "ブログ作成しました!"
+    else
+      render :new
+    end
+  end
   def index
+    @tasks = Task.all
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました!"
+    else
+      render :edit
+    end
+  end
+  private
+  def task_params
+    params.require(:task).permit(:name, :content)
+  end
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  # get '/tasks', to: 'tasks#index'
-  root :to => 'tasks#index'
+  root to: 'tasks#index'
+  resources :tasks
 end
