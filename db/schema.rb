b784b688f8741510366ac944b8cbe13ef25b40bf
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-22 14:45:21 +0900

    タスクの一覧画面、詳細画面を作成する

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_045810) do
+ActiveRecord::Schema.define(version: 2021_12_22_050900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "content"
+    t.string "name", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
