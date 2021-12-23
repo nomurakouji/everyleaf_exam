d2b0b8615ced19b357660348f2a3c9f509c0cb50
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-23 11:44:52 +0900

    system_specの「タスクを新規作成したとき、作成したタスクが画面に表示される」

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,7 +2,31 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
-      it '作成したタスクが表示される' do
+      before do
+        Task.create!(name: 'タスク名', content:'歯をみがく')
+      end
+        it '作成したタスクが表示される' do
+      # 1. new_task_pathに遷移する（新規作成ページに遷移する）
+      # ここにnew_task_pathにvisitする処理を書く
+      visit new_task_path
+      # 2. 新規登録内容を入力する
+      #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
+      #?????????位置はitの下ではないの？？？？？
+      # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+      fill_in 'Name', with: 'タスク名'
+                            #   task = FactoryBot.create(:task, name: 'タスク名')
+      # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+      fill_in 'Content', with: '歯をみがく'
+                            #task = FactoryBot.create(:task, content: '歯をみがく')
+      # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
+      # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+    #   <input type="submit" name="commit" value="Create Task">
+      click_button 'Create Task'
+      # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
+      # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
+      # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+      expect(page).to have_content 'タスク名'
+      expect(page).to have_content '歯をみがく'
       end
     end
   end
