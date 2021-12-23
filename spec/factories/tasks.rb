f7a0e85ea63d7f70d89b5c833216b7ae2971b2a7
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-23 20:45:30 +0900

    Model_Specの3要件

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
-    factory :task do
-      name { 'test_name' }
-      content { 'test_content' }
-    end
+  factory :task do
+    name { 'test_name' }
+    content { 'test_content' }
   end
+  factory :second_task, class: Task do
+    name {'test_name'}
+    content { '' }
+  end
+end
