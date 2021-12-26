56060bb1e6fbff600586cfaa4390d1215eb32438
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-26 20:00:32 +0900

    タスクの終了期限

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   validates :name, presence:true
   validates :content, presence:true
-  default_scope -> { order(created_at: :desc) }
+  # default_scope -> { order(created_at: :desc) }
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  before do
-  FactoryBot.create(:task)
-  FactoryBot.create(:second_task)
-  visit tasks_path
-  end
-  describe '一覧表示機能' do
-    context '一覧画面に遷移した場合' do
-      it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, name: 'task')
-        visit tasks_path
-        expect(page).to have_content 'task'
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      before do
+        Task.create!(name: 'taro', content:'aaa', deadline:'2021年12月1日')
       end
-    end
-    context 'タスクが作成日時の降順に並んでいる場合' do
-      it '新しいタスクが一番上に表示される' do
-        assert Task.all.order(created_at: "desc") 
+      it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in 'task[name]', with: 'taro'
+        fill_in 'task[content]', with: 'aaa'
+        fill_in 'task[deadline]', with: '2021年12月1日'
+        click_on '登録する'
+        expect(page).to have_content 'taro'
+        expect(page).to have_content 'aaa'
+        expect(page).to have_content '2021年12月1日'
       end
     end
   end
