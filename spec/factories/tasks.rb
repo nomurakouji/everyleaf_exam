6f3085b5c814355dbb86784eeacb775046a3d4a1
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-26 20:00:32 +0900

    タスクの終了期限

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :task do
-    name { 'タスク１' }
+    name { 'taro' }
     content { 'コンテント１' }
+    deadline{'2021年11月１日'}
   end
-  factory :second_task, class: Task do
-    name { 'タスク２' }
-    content { 'コンテント２' }
-  end
+  # factory :second_task, class: Task do
+  #   name { 'タスク２' }
+  #   content { 'コンテント２' }
+  # end
 end
