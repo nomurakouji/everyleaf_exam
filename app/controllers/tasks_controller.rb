25636963fb4cc7e8bfb9410cb3db7aee1a478fa1
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-26 23:57:39 +0900

    一覧画面でタスクを終了期限でソート

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,11 @@ class TasksController < ApplicationController
     end
   end
   def index
-    @tasks = Task.all
+    if params[:sort_expired]
+      @tasks = Task.all.order(deadline: "DESC")
+    else
+      @tasks = Task.all
+    end
   end
   def show
   end
@@ -31,7 +35,7 @@ class TasksController < ApplicationController
   end
   private
   def task_params
-    params.require(:task).permit(:name, :content)
+    params.require(:task).permit(:name, :content, :deadline)
   end
   def set_task
     @task = Task.find(params[:id])
