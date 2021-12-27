c7ebdb41b2908e5a4eafbc37ab891bdce27daef7
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-27 16:16:00 +0900

    終了期限のテストをsystem_specで実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,11 +1,17 @@
 FactoryBot.define do
   factory :task do
-    name { 'taro' }
-    content { 'コンテント１' }
-    deadline{'2021年11月１日'}
+    name { 'タスク1' }
+    content { 'コンテント1' }
+    deadline {'2000/1/1'}
   end
-  # factory :second_task, class: Task do
-  #   name { 'タスク２' }
-  #   content { 'コンテント２' }
-  # end
+  factory :second_task, class: Task do
+    name { 'タスク2' }
+    content { 'コンテント2' }
+    deadline {'2021/12/27'}
+  end
+  factory :third_task, class: Task do
+    name {'タスク3'}
+    content {'コンテント3'}
+    deadline {'1111/11/11'}
+  end
 end
