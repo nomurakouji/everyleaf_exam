812b0cee0972337aba50cabfc6de353c4cbdac38
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-31 12:51:43 +0900

    system_specのステータス登録、タイトル検索テスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,15 +3,18 @@ FactoryBot.define do
     name { 'タスク1' }
     content { 'コンテント1' }
     deadline {'2000/1/1'}
+    condition {'未着手'}
   end
   factory :second_task, class: Task do
     name { 'タスク2' }
     content { 'コンテント2' }
     deadline {'2021/12/27'}
+    condition {'着手中'}
   end
   factory :third_task, class: Task do
     name {'タスク3'}
     content {'コンテント3'}
     deadline {'1111/11/11'}
+    condition {'完了'}
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -12,19 +12,28 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'タスク1'
         expect(page).to have_content 'コンテント1'
         expect(page).to have_content '2000/1/1'
+        expect(page).to have_content '未着手'
         expect(page).to have_content 'タスク2'
         expect(page).to have_content 'コンテント2'
         expect(page).to have_content '2021/12/27'
+        expect(page).to have_content '着手中'
       end
     end
-    context 'タスクが終了期限の降順で並んでいる場合' do
-      it '最新の終了期限のタスクが一番上に表示される' do
-        click_link "終了期限でソートする"
-        get_deadline = all('.task_deadline')
-        expect(get_deadline[0]).to have_content '2021/12/27'
-        expect(get_deadline[1]).to have_content '2000/1/1'
-        expect(get_deadline[2]).to have_content '1111/11/11'
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        fill_in "name" , with: "1"
+        click_on 'search'
+        expect(page).to have_content '1'
       end
     end
+    # context 'タスクが終了期限の降順で並んでいる場合' do
+    #   it '最新の終了期限のタスクが一番上に表示される' do
+    #     click_link "終了期限でソートする"
+    #     get_deadline = all('.task_deadline')
+    #     expect(get_deadline[0]).to have_content '2021/12/27'
+    #     expect(get_deadline[1]).to have_content '2000/1/1'
+    #     expect(get_deadline[2]).to have_content '1111/11/11'
+    #   end
+    # end
   end
 end
