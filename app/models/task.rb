74eb7fee4fb032d34123977b945630ea00123c50
kouji nomura <nomurakouji0711@yahoo.co.jp>
2022-01-01 12:39:17 +0900

    modelスペックのテストを実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,14 +2,14 @@ class Task < ApplicationRecord
   validates :name, presence:true
   validates :content, presence:true
   enum condition: { 未着手: '未着手', 着手中: '着手中', 完了: '完了' }
-  scope :task_name_search, -> (query) {where("name LIKE ?", "%#{query}%")}
-    def task_name_search(query)
-      where("name LIKE ?", "%#{query}%")
-    end
+  scope :task_name_search, -> (name) {where("name LIKE ?", "%#{name}%")}
+    # def task_name_search(query)
+    #   where("name LIKE ?", "%#{query}%")
+    # end
   
-  scope :condition_search, -> (query) {where(condition: query)}
-    def condition_search(query)
-      where(condition: query)
-    end
+  scope :condition_search, -> (condition) {where(condition: condition)}
+    # def condition_search(query)
+    #   where(condition: query)
+    # end
 end
 
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,22 +1,30 @@
 require 'rails_helper'
+
 RSpec.describe 'タスクモデル機能', type: :model do
-  describe 'バリデーションのテスト' do
-    context 'タスクのタイトルが空の場合' do
-      it 'バリデーションにひっかる' do
-        task = Task.new(name: '', content: '失敗テスト')
-        expect(task).not_to be_valid
+  describe '検索機能' do
+    let!(:task){ FactoryBot.create(:task)}
+    let!(:second_task){ FactoryBot.create(:second_task)}
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        expect(Task.task_name_search('タスク1')).to include(task)
+        expect(Task.task_name_search('タスク1')).not_to include(second_task)
+        expect(Task.task_name_search('タスク1').count).to eq 1
       end
     end
-    context 'タスクの詳細が空の場合' do
-      it 'バリデーションにひっかかる' do
-        task = Task.new(name: '失敗テスト', content: ' ')
-        expect(task).not_to be_valid
+
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.condition_search('未着手')).to include(task)
+        expect(Task.condition_search('未着手')).not_to include(second_task)
+        expect(Task.condition_search('未着手').count).to eq 1
       end
     end
-    context 'タスクのタイトルと詳細に内容が記載されている場合' do
-      it 'バリデーションが通る' do
-        task = Task.new(name: '早起き', content: '７時３０分まで')
-        expect(task).to be_valid
+
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.task_name_search('タスク1')).to include(task)
+        expect(Task.condition_search('未着手')).to include(task)
+        expect(Task.task_name_search('タスク1').count).to eq 1
       end
     end
   end
