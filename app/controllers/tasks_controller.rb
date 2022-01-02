fe65710be9d17b18b913d47ca44e33c68d74f887
kouji nomura <nomurakouji0711@yahoo.co.jp>
2022-01-02 11:50:14 +0900

    優先順位の登録

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,7 +52,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :content, :deadline, :condition)
+    params.require(:task).permit(:name, :content, :deadline, :condition, :priority)
   end
 
   def set_task
