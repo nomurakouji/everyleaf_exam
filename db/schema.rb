ddf14ca297fe3c8d7696f28935c8eb703bb82677
kouji nomura <nomurakouji0711@yahoo.co.jp>
2022-01-05 19:32:57 +0900

    一対多アソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_081756) do
+ActiveRecord::Schema.define(version: 2022_01_05_083821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2022_01_05_081756) do
     t.string "deadline"
     t.string "condition"
     t.integer "priority"
+    t.bigint "user_id", null: false
+    t.index ["name", "content", "deadline", "condition"], name: "index_tasks_on_name_and_content_and_deadline_and_condition"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_01_05_081756) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
