800f9f51db4a4c502b99426e142f31d1a60032ea
kouji nomura <nomurakouji0711@yahoo.co.jp>
2022-01-06 13:24:02 +0900

    自分が作成したタスクだけ表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,7 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+    @task.user_id = current_user.id
     if @task.save
       redirect_to tasks_path, notice: "タスク作成しました!"
     else
@@ -18,22 +19,22 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.page(params[:page]).per(5)
     if params[:sort_expired]
-      @task = Task.all.order(deadline: :desc).page(params[:page]).per(5)
+      @task = current_user.tasks.order(deadline: :desc).page(params[:page]).per(5)
 
     elsif params[:sort_priority]
-      @task = Task.all.order(priority: :asc).page(params[:page]).per(5)
+      @task = current_user.tasks.order(priority: :asc).page(params[:page]).per(5)
     
 
     elsif params[:name].present?
       if params[:condition].present?
-        @task = Task.all.task_name_search(params[:name]).condition_search(params[:condition]).page(params[:page]).per(5)
+        @task = current_user.tasks.task_name_search(params[:name]).condition_search(params[:condition]).page(params[:page]).per(5)
       else
-        @task = Task.all.task_name_search(params[:name]).page(params[:page]).per(5)
+        @task = current_user.tasks.task_name_search(params[:name]).page(params[:page]).per(5)
       end
     elsif params[:condition].present?
-      @task = Task.all.condition_search(params[:condition]).page(params[:page]).per(5)
+      @task = current_user.tasks.condition_search(params[:condition]).page(params[:page]).per(5)
     else 
-      @task = Task.all.order(condition: :desc).page(params[:page]).per(5)
+      @task = current_user.tasks.order(condition: :desc).page(params[:page]).per(5)
     end
   end
 
