c341c476f7caa110aba14aba5ebda006c2d48cae
kouji nomura <nomurakouji0711@yahoo.co.jp>
2022-01-08 12:57:13 +0900

    容量オーバー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @users = User.all
+    @tasks = @user.tasks
     if @user.id == @current_user.id
       render "show"
     elsif logged_in?
