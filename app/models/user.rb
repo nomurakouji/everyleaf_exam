fe240b7031af8c8b98fac6234a388b4f4d298015
kouji nomura <nomurakouji0711@yahoo.co.jp>
2022-01-09 12:09:51 +0900

    管理ユーザが一人もいなくなってしまわないように、モデルのコールバックを利用して更新・削除の制御をする

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   has_secure_password
   validates :namae, presence:true
   validates :email, presence:true
+
+  before_update :admin_update_check
+  before_destroy :admin_exist_check
+  
+  def admin_update_check
+    if User.where(admin: true).count == 1 && self.admin == false && self.changes[:admin].nil? == false
+      throw(:abort) 
+    end
+  end
+
+  def admin_exist_check
+    if User.where(admin: true).count == 1 && self.admin == true
+      throw(:abort)
+    end
+  end
 end
