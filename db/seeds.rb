b7f8133ee0060cc4183d975e401f0c8062e7ad9b
kouji nomura <nomurakouji0711@yahoo.co.jp>
2022-01-14 11:50:46 +0900

    seedデータ/user投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create!(
-    namae: 'admin',
-    email: 'admin@admin.com',
-    password: 'admin@admin.com',
-    password_confirmation: 'admin@admin.com',
-    admin: 'true'
-)
+# User.create!(
+#     namae: 'admin',
+#     email: 'admin@admin.com',
+#     password: 'admin@admin.com',
+#     password_confirmation: 'admin@admin.com',
+#     admin: 'true'
+# )
+
+# 9.times do |n|
+#   namae = Faker::Games::Pokemon.name
+#   email = Faker::Internet.email
+#   password = "password"
+#   password_confirmation = "password"
+#   admin = false
+#   User.create!(namae: namae,
+#                email: email,
+#                password: password,
+#                password_confirmation: password_confirmation,
+#                admin: admin,
+#                )
+# end
+
+10.times do |n|
+    name = Faker::Name.name 
+    content = Faker::Currency.name
+    deadline = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :long)
+    condition = "完了"
+    priority = "低"
+    Task.create!(name: name,
+                 content: content,
+                 deadline: deadline,
+                 condition: condition,
+                 priority: priority,
+                 )
+  end
+
+  10.times do |n|
+    label = Faker::Color.color_name
+    Tag.create!(label: label,
+                 )
+  end
